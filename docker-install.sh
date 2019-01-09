f479dca880277479befe07370d4fcc01a19cf2c5
fukuda <fukudammk@gmail.com>
2019-01-10 01:49:16 +0900

    Add script

---
+++ docker-install.sh
@@ -0,0 +1,7 @@
+yum -y remove docker docker-common docker-selinux docker-engine
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum makecache fast
+yum install docker-ce
+systemctl enable docker
+systemctl start docker
