33a2b21afe987c66334ac3c7bfeb76d1573da4ba
fukuda <fukudammk@gmail.com>
2019-01-10 02:10:07 +0900

    Add docker-compose install

--- docker-install.sh
+++ docker-install.sh
@@ -1,3 +1,5 @@
+# install docker
+
 yum -y remove docker docker-common docker-selinux docker-engine
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -5,3 +7,9 @@ yum makecache fast
 yum install docker-ce
 systemctl enable docker
 systemctl start docker
+
+
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` > docker-compose
+mv docker-compose /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
