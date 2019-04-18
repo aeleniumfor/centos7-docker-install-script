7b450e1e812d1da48bd727d7d9d5c9bd7c7fa28b
fukuda Ryusei <huku8121@gmail.com>
2019-04-19 03:02:19 +0900

    docker ^y

--- docker-install.sh
+++ docker-install.sh
@@ -3,7 +3,7 @@ yum -y remove docker docker-common docker-selinux docker-engine
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
-yum install docker-ce
+yum -y install docker-ce
 systemctl enable docker
 systemctl start docker
 
